40bfbba8f8a415b11b3bb49a85f1dbcaab7a77b8
Anna <119071299+heyanja@users.noreply.github.com>
2024-05-13 11:36:05 -0400

    Initial Commit

---
+++ GreenSquare/GreenSquareApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenSquareApp.swift
+//  GreenSquare
+//
+//  Created by Anna Fadieieva on 2024-05-13.
+//
+
+import SwiftUI
+
+@main
+struct GreenSquareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
